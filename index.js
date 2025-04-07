250896f149742c545b818ae2deae6cf59db4106d
sandu-simon <ssandoo@mail.ru>
2025-04-08 02:03:39 +0300

    Order tab working
    You can add and remove items from arr

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import  { menuArray } from './data.js'
 
+const orderArray=[]
+
 function getMenuHtml(){
     let menuHtml = ''
     menuArray.forEach(function(item){
@@ -12,7 +14,7 @@ function getMenuHtml(){
                 <p class="item-price">$${item.price}</p>
             </div>
             <div class="order-btn">
-                <p class="plus-btn">+</p>
+                <p class="plus-btn" data-id="${item.id}">+</p>
             </div>
         </div>`
     })
@@ -20,9 +22,72 @@ function getMenuHtml(){
     return menuHtml
 }
 
+document.addEventListener('click', function(e){
+    if (e.target.classList.contains('plus-btn')) {
+        const itemId = parseInt(e.target.dataset.id)
+        addItem(itemId)
+        render(); 
+    }
+    if (e.target.classList.contains('remove-btn')) {
+        const itemId = parseInt(e.target.dataset.id)
+        removeItem(itemId)
+        render(); 
+    }
+})
 
-function render(){
-    document.getElementById('food-menu').innerHTML = getMenuHtml();
+function addItem(itemId){
+    const itemObj = menuArray.find(food => food.id === itemId)
+    
+    if (itemObj) {
+        if(!orderArray.includes(itemObj)){
+            orderArray.push(itemObj)
+            document.getElementById('order').style.display = "block";
+
+        } 
+    }
 }
 
-render();
+function removeItem(itemId){
+    const index = orderArray.findIndex(item => item.id === itemId)
+
+    if (index !== -1) {
+        orderArray.splice(index, 1)
+        if(!orderArray[0]){
+            document.getElementById('order').style.display = "none";
+        }
+    }
+}
+
+function getOrderHtml(){
+    let orderHtml = ''
+
+    orderArray.forEach(function(item){
+        orderHtml += `                      
+            <div class="order-item">
+                <p>${item.name}</p>
+                <p class="remove-btn" data-id="${item.id}">remove</p>
+                <p class="order-item-price">$${item.price}</p>
+            </div>`
+    })
+
+    orderHtml += `                    
+        <div class="order-item total-price">
+            <p>Total price</p>
+            <p class="order-item-price">$${
+                orderArray.reduce(function(total,next){
+                return total+next.price
+            },0)}</p>
+        </div>`
+    
+
+    return orderHtml
+}
+
+
+
+function render() {
+    document.getElementById('food-menu').innerHTML = getMenuHtml();
+    document.getElementById('item-list').innerHTML = getOrderHtml();
+}
+
+render(); 
